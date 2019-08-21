0cc77ad0922253108d8e4d0e23d0c4655deb8e0f
Keith M Cooper <keith.cooper@cefas.co.uk>
2019-08-21 15:44:35 +0100

    Update results table

--- SE STAT SIG V15.R
+++ SE STAT SIG V15.R
@@ -468,16 +468,113 @@ str(data3)
 
 #############################################################
 #21/08/2019
-
+## Repeat data so outputs can be produced for PIZ, SIZ REF
 data3mod=data3
 data3mod$site <- data3mod$Treatment
 datatest=rbind(data3,data3mod)
 dim(datatest)
 names(datatest)
 
-data3=datatest
+## Repeat data so outputs can be produced for subregion PIZ, SIZ REF
+data3sub <- data3
+data3sub$site <- paste(data3sub$Sub_region,data3sub$Treatment)
+datatest2=rbind(datatest,data3sub)
+data3=datatest2
+View(data3)
 #############################################################
+## 21/08/2019 pm Get object data3 into sensible order
+## Reorder rows
+str(data3)
+target <- data3$site
+target
+## Change order of treatments
 
+
+
+target <- c("PIZ",
+            "SIZ",
+            "REF",
+            
+            "West IOW PIZ",
+            "West IOW SIZ",
+            "West IOW REF",
+            "East IOW PIZ",
+            "East IOW SIZ",
+            "East IOW REF",
+            "Owers PIZ",
+            "Owers SIZ",
+            "Owers REF",
+            "Hastings PIZ",
+            "Hastings SIZ",
+            "Hastings REF",
+            "127 PIZ",    
+            "137 PIZ",
+            "340 PIZ",
+            "351 PIZ",
+            "372/1 PIZ",
+            "395/1 PIZ",
+            "395/2 PIZ",
+            "396/1 PIZ",
+            "407 PIZ",
+            "435/1 PIZ",
+            "435/2 PIZ",            
+            "451 PIZ",
+            "460 PIZ",
+            "488 PIZ",
+            "500/3 PIZ",
+            
+            
+            
+            
+            
+            
+            "127 SIZ",
+            "137 SIZ",
+            "340 SIZ",
+            "351 SIZ",
+            "372/1 SIZ",
+            "395/1 SIZ",
+            "395/2 SIZ",
+            "396/1 SIZ",
+            "407 SIZ",
+            "435/1 SIZ",
+            "435/2 SIZ",
+            "451 SIZ", 
+            "453 PIZ",
+            "460 SIZ",
+            "488 SIZ",
+            "500/3 SIZ",
+            
+            
+            
+            
+            
+            
+            
+            
+            "NA REF" , 
+            "Context REF",
+            "Box 1 REF", 
+            "Box 2 REF", 
+            "Box 3 REF", 
+            "Box 4 REF", 
+            "Box 5 REF", 
+            "Box 6 REF" 
+            
+)
+
+require(gdata)
+data3$site <- reorder.factor(data3$site, new.order=target)
+require(dplyr)
+data4 <- data3 %>%
+  arrange(site)
+
+View(data4)
+
+data3=data4
+
+
+##############################################
 #### PIZ SED SUMMARY BASELINE/MONITORING ####
 ## Get summary data by area
 #library(plyr)
@@ -564,64 +661,7 @@ pmatrix2
 View(pmatrix2)
 
 
-## Reorder rows
 
-target <- pmatrix2$site
-target
-## Change order of treatments
-target <- c("PIZ",
-            "127 PIZ",    
-            "137 PIZ",
-            "340 PIZ",
-             "351 PIZ",
-            "372/1 PIZ",
-            "395/1 PIZ",
-            "395/2 PIZ",
-            "396/1 PIZ", 
-            "407 PIZ",
-            "435/1 PIZ",
-            "435/2 PIZ",
-           "451 PIZ",
-           "460 PIZ",
-           "488 PIZ",
-           "500/3 PIZ",
-            
-            "SIZ",
-            "127 SIZ",
-            "137 SIZ",
-            "340 SIZ",
-           "351 SIZ",
-           "372/1 SIZ",
-           "395/1 SIZ",
-           "395/2 SIZ",
-           "396/1 SIZ",
-            "407 SIZ",
-            "435/1 SIZ",
-            "435/2 SIZ",
-            "451 SIZ", 
-            "453 PIZ",  
-            "460 SIZ",
-            "488 SIZ", 
-            "500/3 SIZ",
-          
-          "REF",
-          "Context REF",
-            "Box 1 REF", 
-            "Box 2 REF", 
-            "Box 3 REF", 
-            "Box 4 REF", 
-            "Box 5 REF", 
-            "Box 6 REF" 
-             
-            )
-
-require(gdata)
-pmatrix2$site <- reorder.factor(pmatrix2$site, new.order=target)
-require(dplyr)
-pmatrix3 <- pmatrix2 %>%
-  arrange(site)
-
-View(pmatrix3)
 ###############################################################################################################
 ###############################################################################################################
 ###############################################################################################################
@@ -651,8 +691,8 @@ class(sumdata_m)
 class(pmatrix2)
 
 ## Stitch together the results of Wilcox tests (df pmatrix2) with baseline and monitoring sed percentages
-piz_pvalues_means=cbind(pmatrix2,sumdata_b,sumdata_m)
-
+piz_pvalues_means=cbind(pmatrix3,sumdata_b,sumdata_m)
+View(piz_pvalues_means)
 ## Change col names
 names(piz_pvalues_means)
 colnames(piz_pvalues_means)=c("site","cG_p","mG_p","fG_p","cS_p","mS_p","fS_p","SC_p","site","time","count","sc_b","fS_b","mS_b","cS_b","fG_b","mG_b","cG_b","site","time",  "count","sc_m","fS_m","mS_m","cS_m","fG_m","mG_m","cG_m")
@@ -661,10 +701,12 @@ colnames(piz_pvalues_means)=c("site","cG_p","mG_p","fG_p","cS_p","mS_p","fS_p","
 names(piz_pvalues_means)
 
 ## Get all to the top
-n_row=nrow(piz_pvalues_means)
+#n_row=nrow(piz_pvalues_means)
 
 ## Get cols in sensible order
 piz_pvalues_means2=piz_pvalues_means[c(n_row,1:n_row-1),c(1,11,8:2,12,22,13,23,14,24,15,25,16,26,17,27,18,28)]
+piz_pvalues_means2=piz_pvalues_means[,c(1,11,8:2,12,22,13,23,14,24,15,25,16,26,17,27,18,28)]
+
 View(piz_pvalues_means2)
 
 ## Calculate change in sed fractions between baseline and monitoring
@@ -678,13 +720,14 @@ piz_pvalues_means2$mG_change <- piz_pvalues_means2$mG_m-piz_pvalues_means2$mG_b
 piz_pvalues_means2$cG_change <- piz_pvalues_means2$cG_m-piz_pvalues_means2$cG_b
 
 ## Take cols of interest: change and p-values
+str(piz_pvalues_means2)
 pizchange <- piz_pvalues_means2[,c(1:9,24:30)]
 View(pizchange)
 
 ## get into order by n
 class(pizchange)
 str(pizchange)
-pizchange <- pizchange[order(-pizchange$count),]
+#pizchange <- pizchange[order(-pizchange$count),]
 
 ## Round p-values to 3 dp
 pizchange$sc_change=round(pizchange$sc_change,1)
@@ -700,7 +743,7 @@ pizchange$cG_change=round(pizchange$cG_change,1)
 pizchange2 <- pizchange
 colnames(pizchange2) <- c("Site", "n","SCp", "fSp", "mSp", "cSp", "fGp", "mGp", "cGp", "SC", "fS", "mS", "cS", "fG", "mG", "cG")
 #pizchange2 <- pizchange2[,c(1:2,10,3,11,4,12,5,13,6,14,7,15,8,16,9)]
-
+View(pizchange2)
 
 ## pUT P-VALES IN BRACKETS
 #pizchange2[,4] <- paste0("(", format(unlist(pizchange2[,4])),")")
@@ -760,417 +803,30 @@ ft <- align(ft,align="left", part="header")
 ft <- align(ft,align="left", part="body")
 
 ##Change number of dp
-col_keys=c(
-  "Site",
-  "n",
-  "SCp",
-  "fSp",
-  "mSp",
-  "cSp",
-  "fGp",
-  "mGp",
-  "cGp",
-  "SC",
-  "fS",
-  "mS",
-  "cS",
-  "fG",
-  "mG",
-  "cG")
-ft <- colformat_num(ft,  col_keys = col_keys,digits = 2)
+#col_keys=c(
+#  "Site",
+#  "n",
+#  "SCp",
+#  "fSp",
+#  "mSp",
+#  "cSp",
+#  "fGp",
+#  "mGp",
+#  "cGp",
+#  "SC",
+#  "fS",
+#  "mS",
+#  "cS",
+#  "fG",
+#  "mG",
+#  "cG")
+#ft <- colformat_num(ft,  col_keys = col_keys,digits = 2)
 
 
 ## Align text in Site column
-ft <- align_text_col(ft, align = "left")
-ft <- align_nottext_col(ft, align = "right")
+#ft <- align_text_col(ft, align = "left")
+#ft <- align_nottext_col(ft, align = "right")
 ft
 
 
 
-#### SIZ MONITORING DATA ####
-## Plot monitoring data points on SIZ polygons
-plot(siz, axes = TRUE)
-points(pts_m,col = "blue", cex = 0.4,pch=20)
-
-## Start with mondat3 - this is the monitoring samples with piz affiliation. You can therefore remove samples from within PIZ
-mondat3_SIZ=mondat3[is.na(mondat3$area_numbe),]#monitoring samples minus those in PIZ
-#View(mondat3_SIZ)
-
-## Drop the cols from mondat3_SIZ that were associated with the PIZ file 
-mondat3_SIZ2=mondat3_SIZ[,1:11]
-#View(mondat3_SIZ2)#PIZ stations removed
-
-## Monitoring sample positions (non PIZ stations)
-pts_msiz=SpatialPoints(mondat3_SIZ2[,(10:9)], 
-                    proj4string = CRS(proj4string(siz)))
-
-plot(siz)
-points(pts_msiz,col = "blue", cex = 0.01,pch=20)
-
-## Get SIZ affiliations for monitoring sample locations
-mon_nonpiz=over(pts_msiz,siz)
-#View(mon_nonpiz)
-
-## Make a df for non PIZ monitoring samples with SIZ affiliation
-mon_nonpiz=cbind(mondat3_SIZ2,mon_nonpiz)
-#View(mon_nonpiz)
-dim(mon_nonpiz)#550 20
-
-## Remove any samples not within a SIZ
-mon_nonpiz2=mon_nonpiz[!is.na(mon_nonpiz$area_numbe),]
-dim(mon_nonpiz2)# 206 20
-#View(mon_nonpiz2)
-
-## Plot points' to make sure all are within SIZ extents
-pts_msiz2=SpatialPoints(mon_nonpiz2[,(10:9)], 
-                     proj4string = CRS(proj4string(siz)))
-plot(siz, axes = TRUE)
-points(pts_msiz2,col = "blue", cex = 0.01,pch=20)
-
-## Produce df for monitoring stations by SIZ. Note that intersecting samples will appear more than once.
-e <- extract(siz, pts_msiz2)
-#View(e)# Note there are 205 point IDs - this is the number of stations within SIZs
-
-## Remove duplicates based on cols "point.ID" and "area_numbe". Duplicates occur where sites have more than once licence holder.
-e2=e[!duplicated(e[c("point.ID","area_numbe")]),]
-dim(e2)
-
-## Now need to merge dfs for monitoring data and list of stations by SIZ.
-# First need to have a common column to do merge. Add col 'point.ID' to df 'mon_nonpiz2'
-mon_nonpiz2$point.ID <- seq.int(nrow(mon_nonpiz2)) # first add a col for point.ID
-names(mon_nonpiz2)
-#mon_nonpiz3=mon_nonpiz2[,c(1:11,30)]
-#View(mon_nonpiz2)
-
-# Now do the merge
-siz_stm=merge(e2,mon_nonpiz2, by="point.ID")
-#View(siz_stm)
-dim(siz_stm)
-
-## Add in unique identifier based on Code and area_numb.x
-siz_stm$ID=paste(siz_stm$Code,siz_stm$area_numbe.x)
-
-
-#### SIZ BASELINE DATA ####
-## Plot monitoring data points on SIZ polygons
-plot(siz, axes = TRUE)
-points(pts_b,col = "red", cex = 0.4,pch=20)
-
-## Start with basdat3 - these are the baseline samples with piz affiliation. You can therefore remove samples from within PIZ
-basdat3_SIZ=basdat3[is.na(basdat3$area_numbe),]#baseline samples minus those in PIZ
-#View(basdat3_SIZ)
-
-## Drop the cols from basdat3_SIZ that were associated with the PIZ file 
-basdat3_SIZ2=basdat3_SIZ[,1:11]
-#View(basdat3_SIZ2)
-
-## Monitoring sample positions (non PIZ stations)
-pts_bsiz=SpatialPoints(basdat3_SIZ2[,(10:9)], 
-                       proj4string = CRS(proj4string(siz)))
-
-plot(siz)
-points(pts_bsiz,col = "red", cex = 0.01,pch=20)
-
-## Get SIZ affiliations for baseline sample locations
-bas_nonpiz=over(pts_bsiz,siz)
-#View(bas_nonpiz)
-
-## Make a df for non PIZ baseline samples with SIZ affiliation
-bas_nonpiz1=cbind(basdat3_SIZ2,bas_nonpiz)
-#View(bas_nonpiz1)
-dim(bas_nonpiz1)#550 20
-
-## Remove any baseline samples not within a SIZ
-bas_nonpiz2=bas_nonpiz1[!is.na(bas_nonpiz1$area_numbe),]
-dim(bas_nonpiz2)# 206 20
-#View(bas_nonpiz2)
-
-## Plot points to make sure all are within SIZ extents
-pts_bsiz2=SpatialPoints(bas_nonpiz2[,(10:9)], 
-                        proj4string = CRS(proj4string(siz)))
-plot(siz, axes = TRUE)
-points(pts_bsiz2,col = "green", cex = 0.01,pch=20)
-
-## Produce df for baseline stations by SIZ. Note that intersecting samples will appear more than once.
-eb <- extract(siz, pts_bsiz2)
-## Note there are 259 point IDs - this is the number of stations within SIZs )good)
-#View(eb)
-
-## Remove duplicates based on cols "point.ID" and "area_numbe"
-eb2=eb[!duplicated(eb[c("point.ID","area_numbe")]),]
-dim(eb2)
-
-## Now need to merge dfs for baseline data and list of stations by SIZ.
-# First need to have a common column to do merge. Add col 'point.ID' to df 'bas_nonpiz2'
-bas_nonpiz2$point.ID <- seq.int(nrow(bas_nonpiz2)) # first add a col for point.ID
-names(bas_nonpiz2)
-#bas_nonpiz3=bas_nonpiz2[,c(1:11,30)]
-#View(bas_nonpiz2)
-
-# Now do the merge
-siz_stb=merge(eb2,bas_nonpiz2, by="point.ID")
-#View(siz_stb)
-dim(siz_stb)
-
-## Add in unique identifier based on Code and area_numb.x
-siz_stb$ID=paste(siz_stb$Code,siz_stb$area_numbe.x)
-
-
-#### SIZ MATCHED MONITORING AND BASELINE DATA ####
-## Select baseline samples where there is an accompanying monitoring sample
-siz_st_b=siz_stb %>%
-  filter(ID %in% siz_stm$ID)
-dim(siz_st_b)# 204 20
-#View(siz_st_b)
-
-
-## Make sure there are no stations in the monitoring set without a corresponding baseline station
-siz_st_m=siz_stm %>%
-  filter(ID %in% siz_st_b$ID)
-siz_st_m
-dim(siz_st_m)
-#View(siz_st_m)
-
-## Now join the baseline df to the monitoring df
-data_SIZ=rbind(siz_st_b,siz_st_m)
-#View(data_SIZ)
-
-## Add column for PIZ
-data_SIZ$Treatment="SIZ"
-
-## Just take the required columns from df data (Code, seds, time, area_numbe,Treatment)
-names(data_SIZ)
-str(data_SIZ)
-#View(data_SIZ)
-data_SIZ2=data_SIZ[,c(21,28:22,31,8,51)]
-#View(data_SIZ2)
-names(data_SIZ2)
-
-## Create a col for Licence no. and PIZ
-data_SIZ2$site <- paste(data_SIZ2$area_numbe.x,data_SIZ2$Treatment)
-names(data_SIZ2)
-
-## Drop cols Treatment and Area_Numbe from df data 2
-data_SIZ3=data_SIZ2[,c(1,8:2,9,12)]
-#View(data_SIZ3)
-
-## Check and update col types
-str(data_SIZ3)
-data_SIZ3$site=as.factor(data_SIZ3$site)
-data_SIZ3$time=as.factor(data_SIZ3$time)
-#View(data_SIZ3)
-summary(data_SIZ3$time)
-
-
-#### SIZ SED SUMMARY BASELINE/MONITORING ####
-## Get summary data by area
-#library(plyr)
-detach("package:plyr", unload=TRUE) 
-#library(dplyr)
-sumdata_SIZ=data_SIZ3%>%
-  group_by(site,time) %>%
-  summarise(
-    count = n(),
-    sc = mean(SC, na.rm = TRUE),  
-    fS = mean(fS, na.rm = TRUE),
-    mS = mean(mS, na.rm = TRUE),
-    cS = mean(cS, na.rm = TRUE),
-    fG = mean(fG, na.rm = TRUE),
-    mG = mean(mG, na.rm = TRUE),
-    cG = mean(cG, na.rm = TRUE))
-sumdata_SIZ
-#View(sumdata_SIZ)
-
-#### SIZ WILCOX TESTS ####
-
-## Take relevant columns
-data5=data_SIZ3[,c(10:9,2:8)]
-data5
-
-## Create a vector for site
-siteSIZ = as.character(data5$site)
-str(data5)
-
-## Identify the number of sites for each site
-table(siteSIZ)
-
-## Vector for site names
-#site.names = c("127 SIZ",   "137 SIZ",   "340 SIZ",   "351 SIZ", "372/1 SIZ", "395/1 SIZ", "395/2 SIZ", "396/1 SIZ", "396/2 SIZ", "435/1 SIZ", "435/2 SIZ",   "451 SIZ",   "453 SIZ",   "460 SIZ",   "488 SIZ", "500/3 SIZ")
-site.namesSIZ =levels(data5$site)
-  
-## Number of sites
-nsitesSIZ = length(site.namesSIZ)# 16
-
-## Matrix for p-values
-pmatrixSIZ = matrix(999, ncol=7, nrow=nsitesSIZ)
-
-## You just do a loop over the N sites. Select out the rows for the jth site (j=1 to N) on each iteration of the loop. And then do the Wilcoxon tests for that site
-for (j in 1:nsitesSIZ) {
-  use = data5[site==site.namesSIZ[j],]
-  for (k in 3:9) {
-    varb = use[,k][use$time=="b"]
-    varm = use[,k][use$time=="m"]
-    pmatrixSIZ[j,(k-2)] = wilcox.test(varb ,varm, alt="two.sided",paired=T)$p.value
-  }
-}
-
-round(pmatrixSIZ,3)
-
-## Change pmatrix from a matrix to a dataframe
-pmatrixSIZ=as.data.frame(pmatrixSIZ)
-pmatrixSIZ
-
-## Add in column names
-colnames(pmatrixSIZ)=c("cG","mG","fG","cS","mS","fS","SC")
-pmatrixSIZ
-
-## Add in row names (site)
-pmatrixSIZ$site=site.namesSIZ
-pmatrixSIZ
-
-## Change order of df
-pmatrixSIZ2=pmatrixSIZ[,c(8,1:7)]
-pmatrixSIZ2
-
-## Round p-values to 3 dp
-pmatrixSIZ2$cG=round(pmatrixSIZ2$cG,3)
-pmatrixSIZ2$mG=round(pmatrixSIZ2$mG,3)
-pmatrixSIZ2$fG=round(pmatrixSIZ2$fG,3)
-pmatrixSIZ2$cS=round(pmatrixSIZ2$cS,3)
-pmatrixSIZ2$mS=round(pmatrixSIZ2$mS,3)
-pmatrixSIZ2$fS=round(pmatrixSIZ2$fS,3)
-pmatrixSIZ2$SC=round(pmatrixSIZ2$SC,3)
-pmatrixSIZ2
-
-#### SIZ CBIND SUMMARY AND WILCOX TEST ####
-
-## Split piz summary data into baseline and monitoring subsets
-sumdata_SIZ_b=sumdata[which(sumdata_SIZ$time=="b"),]
-sumdata_SIZ_m=sumdata[which(sumdata_SIZ$time=="m"),]
-
-## Check same number of columns
-dim(sumdata_SIZ_b)#16 10
-dim(sumdata_SIZ_m)# 16 10
-dim(pmatrixSIZ2)# 16 8
-
-
-## Make sure all objects are class df
-sumdata_SIZ_b=as.data.frame(sumdata_SIZ_b)#16 10
-sumdata_SIZ_m=as.data.frame(sumdata_SIZ_m)# 16 10
-class(sumdata_SIZ_b)#16 10
-class(sumdata_SIZ_m)
-class(pmatrixSIZ2)
-
-## Stitch together the results of Wilcox tests (df pmatrix2) with baseline and monitoring sed percentages
-siz_pvalues_means=cbind(pmatrixSIZ2,sumdata_SIZ_b,sumdata_SIZ_m)
-
-## Change col names
-names(siz_pvalues_means)
-colnames(siz_pvalues_means)=c("site","cG_p","mG_p","fG_p","cS_p","mS_p","fS_p","SC_p","site","time","count","sc_b","fS_b","mS_b","cS_b","fG_b","mG_b","cG_b","site","time",  "count","sc_m","fS_m","mS_m","cS_m","fG_m","mG_m","cG_m")
-
-#View(piz_pvalues_means)
-names(siz_pvalues_means)
-
-## Get cols in sensible order
-siz_pvalues_means2=siz_pvalues_means[,c(1,11,8:2,12,22,13,23,14,24,15,25,16,26,17,27,18,28)]
-View(siz_pvalues_means2)
-
-#### JOIN PIZ AND SIZ RESULTS
-options(scipen=999)# stop sci notation
-results=rbind(piz_pvalues_means2,siz_pvalues_means2)
-View(results)
-names(results)
-
-## Round p-values to 3 dp
-results$SC_p=round(results$SC_p,3)
-results$fS_p=round(results$fS_p,3)
-results$mS_p=round(results$mS_p,3)
-results$cS_p=round(results$cS_p,3)
-results$fG_p=round(results$fG_p,3)
-results$mG_p=round(results$mG_p,3)
-results$cG_p=round(results$cG_p,3)
-results$SC_b=round(results$SC_b,3)
-results$fS_b=round(results$fS_b,3)
-results$mS_b=round(results$mS_b,3)
-results$cS_b=round(results$cS_b,3)
-results$fG_b=round(results$fG_b,3)
-results$mG_b=round(results$mG_b,3)
-results$cG_b=round(results$cG_b,3)
-results$SC_m=round(results$SC_m,3)
-results$fS_m=round(results$fS_m,3)
-results$mS_m=round(results$mS_m,3)
-results$cS_m=round(results$cS_m,3)
-results$fG_m=round(results$fG_m,3)
-results$mG_m=round(results$mG_m,3)
-results$cG_m=round(results$cG_m,3)
-View(results)
-names(results)
-
-## get p-values next to baseline and monitoring data for each sed fraction
-results2=results[,c(1,2,3,10,11,4,12,13,5,14,15,6,16,17,7,18,19,8,20,21,9,22,23)]
-View(results2)
-
-
-####  CONTEXT DATA ####
-## Plot monitoring data points on PIZ polygons
-plot(piz, axes = TRUE)
-points(pts_m,col = "blue", cex = 0.4,pch=20)
-
-## ID samples within PIZ
-con_piz=over(pts_m,piz)
-View(con_piz)
-
-con_siz==over(pts_m,siz)
-
-con_piz_siz=cbind(as.data.frame(con_piz$area_numbe,con_siz$area_numbe))
-
-#####################################################################################
-tb <- table(e[,1])
-i <- as.integer(names(tb[tb>1]))
-points(pts_msiz2[i,], pch=20, col='purple')
-
-e
-
-
-
-
-##############################
-
-
-## https://gis.stackexchange.com/questions/201231/point-in-overlapping-polygons-using-r
-
-library(raster)
-n <- 100
-set.seed(0)
-x <- runif(n) * 360 - 180
-y <- runif(n) * 180 - 90
-xy <- cbind(x, y)
-p1 <- rbind(c(-180,-20), c(-140,55), c(10, 0), c(-140,-60), c(-180,-20))
-hole <- rbind(c(-150,-20), c(-100,-10), c(-110,20), c(-150,-20))
-p1 <- list(p1, hole)
-p2 <- rbind(c(-10,0), c(140,60), c(160,0), c(140,-55), c(-10,0))
-p3 <- rbind(c(-125,0), c(0,60), c(40,5), c(15,-45), c(-125,0))
-pols <- spPolygons(p1, p2, p3)
-
-plot(pols, col=rainbow(3, alpha=.5),axes = TRUE)
-points(xy)
-
-e <- extract(pols, xy)
-
-e
-
-
-
-tb <- table(e[,1])
-i <- as.integer(names(tb[tb>1]))
-points(xy[i,], pch=20, col='red')
-
-e
-##############################################################################################
-## Plot SIZs
-plot(siz,axes = TRUE)
-
-## Overlay the monitoring stations
-points(pts_m,col = "blue", cex = 0.4,pch=20)
-
-e <- extract(siz, pts_m)      
